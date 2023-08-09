4d576bbfb0173ef1393f6be9802155a9337bcde7
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-08-09 14:20:22 +0200

    Add scss to prettier formatting file

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -10,7 +10,10 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "svelte" }, extra_filetypes = { "toml" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with {
+    filetypes = { "html", "markdown", "css", "svelte", "scss" },
+    extra_filetypes = { "toml" },
+  }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
