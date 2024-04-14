fee1e323b2c9c8dd8466b84244e9a3a20c21ceb4
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2024-04-14 12:24:48 +0200

    Remove auto close

--- lua/plugins/extras/debugging.lua
+++ lua/plugins/extras/debugging.lua
@@ -11,12 +11,6 @@ local spec = {
 		dap.listeners.after.event_initialized["dapui_config"] = function()
 			dapui.open()
 		end
-		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
-		end
 
 		map("n", "<leader>dq", dapui.close, { desc = "Close the UI" })
 	end,
