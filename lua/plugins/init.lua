d3c4a8f15dc2b6a8e08ed05741e62ccfabb9daa8
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2024-06-03 13:08:37 +0200

    Add neogit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -146,5 +146,18 @@ return {
 		end,
 	},
 
+	{
+		"NeogitOrg/neogit",
+    cmd = { "Neogit" },
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+	},
+
 	{ import = "plugins.other" },
 }
