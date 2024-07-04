34caccc92348159eb540d45bf712a84293d45361
Henrik Tøn Løvhaug <henrik.lovhaug@noaignite.com>
2024-07-04 10:36:09 +0200

    Add todo plugin

---
+++ lua/plugins/other/todo.lua
@@ -0,0 +1,12 @@
+local spec = {
+	"folke/todo-comments.nvim",
+  lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
+
+return spec
