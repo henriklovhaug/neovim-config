efcc9f39f9239e6abb8868701c6ffcf393295071
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2025-04-16 12:35:42 +0200

    Reduce ltex noise

--- lua/options.lua
+++ lua/options.lua
@@ -13,3 +13,24 @@ o.incsearch = true
 vim.diagnostic.config({
 	update_in_insert = false,
 })
+
+vim.lsp.handlers['$/progress'] = function(_, result, ctx)
+    local value = result.value
+    if not value.kind then
+        return
+    end
+
+    local client_id = ctx.client_id
+    local name = vim.lsp.get_client_by_id(client_id).name
+
+    if name == 'ltex-plus' then
+        if result.token == ltex_token then
+            return
+        end
+        if value.title == 'Checking document' then
+            ltex_token = result.token
+            return
+        end
+    end
+
+end
