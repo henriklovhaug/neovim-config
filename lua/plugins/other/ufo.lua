8ebce9058f6183350cc2a931fcca23af74ae30db
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2025-04-26 09:43:35 +0200

    Add keymaps to ufo

--- lua/plugins/other/ufo.lua
+++ lua/plugins/other/ufo.lua
@@ -39,6 +39,8 @@ local plugin = {
 		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+		vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
 		vim.keymap.set("n", "F", function()
 			local winid = require("ufo").peekFoldedLinesUnderCursor()
 			if not winid then
