3538aef0b2a1df290e9c0f72c0074ddb8a2c9fcb
Henrik Tøn Løvhaug <henrik.lovhaug@ontimenet.com>
2026-01-28 11:28:38 +0100

    Fix warning nvim_tree and fix masoninstall

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -29,6 +29,100 @@ M.ui = {
 	},
 }
 
+M.mason = {
+	pkgs = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
+
+		-- web dev stuff
+		"astro-language-server",
+		"css-lsp",
+		"deno",
+		"djlint",
+		"html-lsp",
+		"htmx-lsp",
+		"prettier",
+		"prettierd",
+		"typescript-language-server",
+
+		--tailwindcss
+		"tailwindcss-language-server",
+
+		--python stuff
+		"debugpy",
+		"isort",
+		"pyright",
+		"ruff",
+
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
+
+		-- rust
+		"codelldb",
+		"rust-analyzer",
+
+		-- svelte
+		"svelte-language-server",
+
+		-- Docker compose
+		"docker-compose-language-service",
+		"dockerfile-language-server",
+		"hadolint",
+
+		-- sql
+		"prisma-language-server",
+		"sqlfmt",
+		"sqls",
+
+		-- go
+		"delve",
+		"glint",
+		"goimports",
+		"goimports-reviser",
+		"golangci-lint",
+		"golangci-lint-langserver",
+		"golines",
+		"gopls",
+		"gotests",
+		"gotestsum",
+
+		-- java
+		"checkstyle",
+		"google-java-format",
+		"java-debug-adapter",
+		"java-test",
+		"jdtls",
+		"vscode-java-decompiler",
+
+		-- latex
+		"latexindent",
+		"texlab",
+		"ltex-ls-plus",
+
+		-- markdown
+		"harper-ls",
+		"ltex-ls",
+		"marksman",
+		"markdown-oxide",
+
+		-- xml
+		"xmlformatter",
+
+		-- cli
+		"ast-grep",
+		"gitui",
+		"trivy",
+
+		-- shell
+		"beautysh",
+
+		-- Pest
+		"pest-language-server",
+	},
+}
+
 M.lsp = { signature = false }
 
 return M
--- lua/overrides.lua
+++ lua/overrides.lua
@@ -50,103 +50,6 @@ M.treesitter = {
 	},
 }
 
-M.mason = {
-	ensure_installed = {
-		-- lua stuff
-		"lua-language-server",
-		"stylua",
-
-		-- web dev stuff
-		"astro-language-server",
-		"css-lsp",
-		"deno",
-		"djlint",
-		"html-lsp",
-		"htmx-lsp",
-		"prettier",
-		"prettierd",
-		"typescript-language-server",
-
-		--tailwindcss
-		"tailwindcss-language-server",
-
-		--python stuff
-		"debugpy",
-		"isort",
-		"mypy",
-		"pyright",
-		"ruff",
-
-		-- c/cpp stuff
-		"clangd",
-		"clang-format",
-
-		-- rust
-		"codelldb",
-		"rust-analyzer",
-
-		-- svelte
-		"svelte-language-server",
-
-		-- Docker compose
-		"docker-compose-language-service",
-		"dockerfile-language-server",
-		"hadolint",
-
-		-- sql
-		"prisma-language-server",
-		"sqlfmt",
-		"sqls",
-
-		-- go
-		"delve",
-		"glint",
-		"goimports",
-		"goimports-reviser",
-		"golangci-lint",
-		"golangci-lint-langserver",
-		"golines",
-		"gopls",
-		"gotests",
-		"gotestsum",
-
-		-- java
-		"checkstyle",
-		"google-java-format",
-		"java-debug-adapter",
-		"java-test",
-		"jdtls",
-		"vscode-java-decompiler",
-
-		-- latex
-		"latexindent",
-		"texlab",
-		"ltex-ls-plus",
-
-		-- markdown
-		"glow",
-		"harper-ls",
-		"ltex-ls",
-		"marksman",
-		"markdown-oxide",
-
-		-- xml
-		"xmlformatter",
-
-		-- cli
-		"ast-grep",
-		"gitui",
-		"trivy",
-
-		-- shell
-		"beautysh",
-
-		-- Pest
-		"pest-language-server",
-	},
-}
-
--- git support in nvimtree
 M.nvimtree = {
 	git = {
 		enable = true,
@@ -165,6 +68,12 @@ M.nvimtree = {
 				git = true,
 			},
 		},
+	},
+	filesystem_watchers = {
+		-- enable = false,
+		max_events = 100000,
+		debounce_delay = 20,
+		ignore_dirs = { "/.ccls-cache", "/build", "/node_modules", "/target", "/build/obj", "/.cache" },
 	},
 }
 
